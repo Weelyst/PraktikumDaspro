f04e0918971a9d0eed9f320fa5be3b233cc42f81
Fizzuz <mhafizgpr@gmail.com>
2025-10-01 18:56:12 +0700

    commit

--- Jobsheet4/TugasKolaborasi17.java
+++ Jobsheet4/TugasKolaborasi17.java
@@ -1,11 +1,5 @@
 public class TugasKolaborasi17{
     public static void main(String[] args) {
-        System.out.println("Haiiiii");
-<<<<<<< HEAD
-        System.out.println("COba pull  ");
-=======
-        System.out.println("COba pull");
-        System.out.println("Aku weelyst");
->>>>>>> c9b09e7211093eb379c2b199dac0cdfe802351dc
+        System.out.println("Haiiii saya upin");
     }
 }
