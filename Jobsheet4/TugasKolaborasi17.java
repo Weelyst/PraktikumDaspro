920bd8f1e43bfaf7b7424f7a5d263c993a2d6cd8
Rose <crowsquare02@gmail.com>
2025-10-01 18:57:26 +0700

    commit

--- Jobsheet4/TugasKolaborasi17.java
+++ Jobsheet4/TugasKolaborasi17.java
@@ -2,5 +2,6 @@ public class TugasKolaborasi17{
     public static void main(String[] args) {
         System.out.println("Haiiiii");
         System.out.println("COba pull");
+        System.out.println("Aku weelyst");
     }
 }
