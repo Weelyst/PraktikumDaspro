80a33e794d7b83589e864823b5351f53603bcad4
Rose <crowsquare02@gmail.com>
2025-10-02 13:15:10 +0700

    commit

--- jobsheet5/ifCetakKRS17.java
+++ jobsheet5/ifCetakKRS17.java
@@ -1,6 +1,6 @@
 package jobsheet5;
 import java.util.Scanner;
-public class ifCetakKRS04{
+public class ifCetakKRS17{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
