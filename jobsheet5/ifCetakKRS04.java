3b16e89a0bf59b7012f39871c4b6fda893e4d77f
Rose <crowsquare02@gmail.com>
2025-10-02 13:26:46 +0700

    commit

---
+++ jobsheet5/ifCetakKRS04.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ifCetakKRS04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak Krs Siakad ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran Terverifikasi");
+            System.out.println("Silahkan cetak KRS0 dan minta Tanda tangan");
+        }
+        
+        
+    }
+}
