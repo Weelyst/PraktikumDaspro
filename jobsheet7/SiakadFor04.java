94cb50d047d2748a046f539ea0b9ce1862db3c17
Rose <crowsquare02@gmail.com>
2025-10-23 12:49:44 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor04.java
+++ jobsheet7/SiakadFor04.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class SiakadFor04 {
     public static void main(String[] args) {
        Scanner sc = new Scanner (System.in);
-       double nilai, tertinggi = 0, terendah = 100;
+       double nilai, tertinggi = 0, terendah = 100, nilaiMinimal = 60;
+       int lulus = 0, tidaklulus = 0;
        for (int i = 1; i <= 10; i++){
         System.out.println("Masukkan nilai Mahasiswa ke-"+ i +": ");
         nilai = sc.nextDouble();
@@ -15,10 +16,20 @@ public class SiakadFor04 {
         if (nilai < terendah){
             terendah = nilai;
         }
+        if (nilai >= nilaiMinimal){
+            lulus++;
+        } else {
+            tidaklulus++;
+        }
+    }
         System.out.println("Nilai Tertinggi: " + tertinggi);
         System.out.println("Nilai terendah " + terendah);
-        
+        System.out.println("Jumlah Mahasiswa Lulus = " + lulus + " Orang");
+        System.out.println("Jumlah Mahasiswa Tidak Lulus = " + tidaklulus + " Orang");
+
+
+        }
+
        }
-    }
     
-}
+    
