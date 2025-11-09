ad663c359e81ca291928c45974436208e6056d3f
Rose <crowsquare02@gmail.com>
2025-11-09 10:20:03 +0700

    tugas 1

--- jobsheet8/tugas1.java
+++ jobsheet8/tugas1.java
@@ -1,5 +1,28 @@
 package jobsheet8;
 
+import java.util.Scanner;
+
 public class tugas1 {
-    //tesstttttt
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int limitN ;
+        String Ntetap = "";
+        int jkdrt = 0;
+        
+        System.out.print("jumlah n : ");
+        limitN = sc.nextInt();
+
+        for (int n = 1; n <= limitN; n++){
+            int angkskrng = n * n;
+            jkdrt  += angkskrng;
+
+            if (n == 1){
+                Ntetap = String.valueOf(angkskrng);
+            }else{
+                Ntetap += " + " + angkskrng;
+            }
+            System.out.println("n = " + n + " -> jumlah kuadrat " + Ntetap + " = " + jkdrt);
+        }
+    }
 }
