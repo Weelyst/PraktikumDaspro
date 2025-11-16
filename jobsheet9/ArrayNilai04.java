3090da37c4506da7e431da76b015447831f41925
Rose <crowsquare02@gmail.com>
2025-11-16 21:36:12 +0700

    percobaan 2

---
+++ jobsheet9/ArrayNilai04.java
@@ -0,0 +1,19 @@
+package jobsheet9;
+import java.util.Scanner;
+public class ArrayNilai04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int [10];
+
+        for (int i = 0; i < 10; i++){
+        System.out.println(("Masukkan nilai akhir ke-"+ i +" ; "));
+        nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai Akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+        
+    }
+
+    
+}
