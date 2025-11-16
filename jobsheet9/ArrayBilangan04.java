de7d098cc2f3577638d8b84bc0488a43bd8e9ab7
Rose <crowsquare02@gmail.com>
2025-11-16 21:46:53 +0700

    modif percobaan 1 reup

--- jobsheet9/ArrayBilangan04.java
+++ jobsheet9/ArrayBilangan04.java
@@ -3,13 +3,11 @@ package jobsheet9;
 public class ArrayBilangan04 {
     
     public static void main(String[] args) {
-       int[] bil = new int[4];
-        
+       int[] bil = new int[4];    
 
-        bil [0] = 5.0;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+      for (int i = 0; i < 4; i++){
+    System.out.println(bil[i]);
+    }
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
