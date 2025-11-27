30fbd10408eb357e17f858b521db2cbb289d87f9
Rose <crowsquare02@gmail.com>
2025-11-27 12:04:01 +0700

    Percobaan 1

---
+++ jobsheet11/Kafe04.java
@@ -0,0 +1,18 @@
+package jobsheet11;
+
+public class Kafe04 {
+    public static void Menu(){
+        System.out.println("==== Menu Resto Kafe ====");
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. ");
+        System.out.println("6. ");
+        System.out.println("=========================");
+        System.out.println(" Silahkan Pilih Menu Yang di pesan ");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
