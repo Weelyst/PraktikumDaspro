d436a64a84dae56828db4701abce0acdcba9f128
Rose <crowsquare02@gmail.com>
2025-11-27 12:20:11 +0700

    Percobaan 2

--- jobsheet11/Kafe04.java
+++ jobsheet11/Kafe04.java
@@ -1,18 +1,23 @@
 package jobsheet11;
 
 public class Kafe04 {
-    public static void Menu(){
+    public static void Menu(String namaPelanggan, boolean isMember){
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember){
+            System.out.println("Anda Adalah Member, Dapatkan diskon 10% untuk setiap Pembelian");
+        }
         System.out.println("==== Menu Resto Kafe ====");
-        System.out.println("1. ");
-        System.out.println("2. ");
-        System.out.println("3. ");
-        System.out.println("4. ");
-        System.out.println("5. ");
-        System.out.println("6. ");
+        System.out.println("1.KoTam     - Rp 15,000 ");
+        System.out.println("2.Capucinno - Rp 20,000");
+        System.out.println("3.Latte     - Rp 22,000");
+        System.out.println("4.Teh Pull  - Rp 12,000");
+        System.out.println("5.Rokar     - Rp 10,000");
+        System.out.println("6.Migor     - Rp 18,000");
         System.out.println("=========================");
         System.out.println(" Silahkan Pilih Menu Yang di pesan ");
     }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 }
