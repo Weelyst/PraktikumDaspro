cea871edd0a3df3bce59f934fae66778ff71c101
Rose <crowsquare02@gmail.com>
2025-11-27 14:51:24 +0700

    Modifikasi Percobaan 2

--- jobsheet11/Kafe04.java
+++ jobsheet11/Kafe04.java
@@ -1,12 +1,23 @@
 package jobsheet11;
 
 public class Kafe04 {
-    public static void Menu(String namaPelanggan, boolean isMember){
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember){
             System.out.println("Anda Adalah Member, Dapatkan diskon 10% untuk setiap Pembelian");
         }
+        System.out.print("Status Promo: ");
+
+        
+
+    if (kodePromo.equals("DISKON50")){
+        System.out.println("Anda mendapatkan diskon 50%! 🎉");
+    } else if (kodePromo.equals("DISKON30")){
+        System.out.println("Anda mendapatkan diskon 30%! 🥳");
+    } else {
+        System.out.println("Kode promo invalid atau tidak digunakan.");
+    }
         System.out.println("==== Menu Resto Kafe ====");
         System.out.println("1.KoTam     - Rp 15,000 ");
         System.out.println("2.Capucinno - Rp 20,000");
@@ -18,6 +29,6 @@ public class Kafe04 {
         System.out.println(" Silahkan Pilih Menu Yang di pesan ");
     }
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "NONPROMO" );
     }
 }
