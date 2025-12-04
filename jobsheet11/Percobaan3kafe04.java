97052c04a85641e8dacee88bd62be75adc1e323b
Rose <crowsquare02@gmail.com>
2025-12-04 15:02:50 +0700

    modif percobaan 3

--- jobsheet11/Percobaan3kafe04.java
+++ jobsheet11/Percobaan3kafe04.java
@@ -6,9 +6,37 @@ public class Percobaan3kafe04 {
     public static int hitungTotalHarga04(int pilihanMenu, int banyakItem){
         int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
 
-        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;
+        if (pilihanMenu >= 1 && pilihanMenu <= hargaItems.length) {
+            int hargatotal = hargaItems[pilihanMenu - 1] * banyakItem;
+            return hargatotal;
+        } else {
+            return 0; 
+        }
     }
+
+     public static int hitungTotalHargaDiskon04(int totalHargaAwal, String kodePromo) {
+        double diskon = 0; 
+        double jumlahDiskonRupiah = 0;
+        
+        System.out.println("\n[PROMO APLIKASI]");
+
+         if (kodePromo.equals("DISKON50")) {
+            diskon = 0.50; 
+            jumlahDiskonRupiah = totalHargaAwal * diskon;
+            System.out.println("Kode Promo: DISKON50 - Diskon 50% Diterapkan!");
+            System.out.println("Diskon yang Anda dapatkan: Rp " + (int)jumlahDiskonRupiah);
+        } else if (kodePromo.equals("DISKON30")) {
+            diskon = 0.30; 
+            jumlahDiskonRupiah = totalHargaAwal * diskon;
+            System.out.println("Kode Promo: DISKON30 - Diskon 30% Diterapkan!");
+            System.out.println("Diskon yang Anda dapatkan: Rp " + (int)jumlahDiskonRupiah);
+        } else {
+            System.out.println("Kode promo '" + kodePromo + "' tidak valid. Tidak ada pengurangan harga.");
+        }
+         int totalHargaAkhir = totalHargaAwal - (int)jumlahDiskonRupiah;
+        return totalHargaAkhir;
+    }
+        
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
         Scanner sc = new Scanner(System.in);
@@ -18,16 +46,6 @@ public class Percobaan3kafe04 {
             System.out.println("Anda Adalah Member, Dapatkan diskon 10% untuk setiap Pembelian");
         }
         System.out.print("Status Promo: ");
-
-        
-    if (kodePromo.equals("DISKON50")){
-        System.out.println("Anda mendapatkan diskon 50%! ");
-    } else if (kodePromo.equals("DISKON30")){
-        System.out.println("Anda mendapatkan diskon 30%! ");
-    } else {
-        System.out.println("Kode promo invalid atau tidak digunakan.");
-    }
-
 
 
         System.out.println("==== Menu Resto Kafe ====");
@@ -48,28 +66,33 @@ public class Percobaan3kafe04 {
         System.out.println("Total harga untuk pesanan Rp" + totalHarga);
 
 
-
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       System.out.println("------------------------------");
-       String nama1 = "Arthur";
-        boolean member1 = true;
-        String promo1 = "DISKON50";
+       System.out.println("~~~~~~~ ~~~~~~~");
+       String namaPelanggan = "Arthur";
+        boolean isMember = true;
+        String kodePromoPelanggan = "DISKON50";
         
-        Menu(nama1, member1, promo1); 
+        Menu(namaPelanggan, isMember, kodePromoPelanggan); 
         
-        System.out.println("------------------------------");
+        System.out.println("~~~~~~~ ~~~~~~~");
         System.out.print("\n Masukkan nomor menu yang dipilih : ");
         int pilihanmenu = sc.nextInt();
 
         System.out.print(" Masukkan banyak item yang dibeli : ");
         int banyakitem = sc.nextInt();
 
-        int totalharga = hitungTotalHarga04(pilihanmenu, banyakitem);
+        int totalHargaAwal = hitungTotalHarga04(pilihanmenu, banyakitem);
+         System.out.println("~~~~~~~ ~~~~~~~-");
+        System.out.println(" Total harga sebelum diskon: Rp " + totalHargaAwal);
 
-        System.out.println(" Total harga yang harus dibayar (sebelum diskon): Rp " + totalharga);
+        int totalHargaFinal = hitungTotalHargaDiskon04(totalHargaAwal, kodePromoPelanggan);
+        
+        System.out.println("\n Total harga yang harus dibayar : Rp " + totalHargaFinal);
+        System.out.println("~~~~~~~ ~~~~~~~");
         
         sc.close();
     }
